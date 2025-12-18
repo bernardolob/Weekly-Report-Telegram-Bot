035f806c6d89419902672f66a157d6eb03169c0b
Bernardo Lobão <bernardocortezdelobao@gmail.com>
2025-12-18 00:32:54 +0000

    some logs

--- task.bat
+++ task.bat
@@ -14,13 +14,13 @@ echo Running weekend task >> C:\Users\berna\Documents\telegram-bot\logs\weekend.
 echo Working dir: %CD% >> .\logs\weekend.log
 
 REM --- Start python in background ---
-start "TelegramBot" python .\main.py
+start "TelegramBot" cmd /c "python .\main.py >> .\logs\telegrambot.log 2>&1"
 
 
 echo Starting timer >> .\logs\weekend.log
 
 REM --- Wait 5 minutes (300 seconds) ---
-timeout /t 300 /nobreak
+timeout /t 30 /nobreak
 
 REM --- Kill python process ---
 taskkill /IM python.exe /F
@@ -29,4 +29,3 @@ echo Killed Python >> .\logs\weekend.log
 REM --- Suspend ---
 echo Shutting Down >> .\logs\weekend.log
 
-shutdown /h
